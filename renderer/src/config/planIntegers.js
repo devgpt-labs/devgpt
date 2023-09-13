38f570d63604765b70485480bcd450cc3f581f40
Ash Lewis <49353972+ash-lewis@users.noreply.github.com>
2023-09-13 18:17:27 +0100

    Update planIntegers.js

--- renderer/src/config/planIntegers.js
+++ renderer/src/config/planIntegers.js
@@ -1,9 +1,9 @@
 //note that we are changing these in our next release to make the free version more powerful.
 const integers = {
-  free_character_count: 1000,
-  paid_character_count: 1000,
-  free_repo_size_count: 250,
-  paid_repo_size_count: 1500,
+  free_character_count: 300,
+  paid_character_count: 300,
+  free_repo_size_count: 2500, 
+  paid_repo_size_count: 2500,
   free_lines_of_code_count: 550,
   paid_lines_of_code_count: 7500,
   is_follow_up_prompts_premium: false,
