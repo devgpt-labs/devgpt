7a34326beed0782392637f53aa4621fa34969ce0
Ash <tom@feb.co.uk>
2023-09-13 18:37:38 +0100

    Added toast for updates

--- renderer/src/components/platform/upgrade/index.tsx
+++ renderer/src/components/platform/upgrade/index.tsx
@@ -1,43 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React, { } from "react";
 import {
-  Box,
-  Text,
-  Slider,
-  SliderTrack,
-  SliderFilledTrack,
   Flex,
   Button,
-  useToast,
   Heading,
 } from "@chakra-ui/react";
-import { supabase } from "@/src/utils/supabaseClient";
-import router from "next/router";
 import { shell } from "electron";
 
 const Plans = () => {
   return (
-    <Flex
-      p={6}
-      w="100vw"
-      h="100vh"
-      alignItems="center"
-      justifyContent={"center"}
-      textAlign={"center"}
-      flexDirection={"column"}
-    >
+    <Flex pos='absolute' top={0} right={0} width={500} height={500} bg='red' >
       <Heading size="xl" mb={3}>
         A new update is available! Please update to the latest version.
       </Heading>
-      <Text size="md" mb={3}>
-        If you're a Mac user your update will complete automatically, please
-        wait. A button will appear soon to allow you to restart the app. You do
-        not have to do anything in the meantime.
-      </Text>
-      <Text size="md" mb={3}>
-        Windows and Linux versions are not yet code signed so you will have to
-        download the latest version manually, below.
-      </Text>
       <Flex flexDirection="row">
         <Button
           mt={10}
@@ -49,7 +23,7 @@ const Plans = () => {
             );
           }}
         >
-          Download the latest version
+          View The Latest Version
         </Button>
         <Button
           mt={10}
@@ -61,7 +35,7 @@ const Plans = () => {
           Join the Discord
         </Button>
       </Flex>
-    </Flex>
+    </Flex >
   );
 };
 export default Plans;
