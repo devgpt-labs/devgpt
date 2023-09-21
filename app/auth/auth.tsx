18d64e5becb15fde93e00790bcbc363dde6e3b10
Ash <tom@feb.co.uk>
2023-09-21 13:44:57 -0700

    Added buttons for git connectors

--- app/auth/auth.tsx
+++ app/auth/auth.tsx
@@ -24,6 +24,27 @@ import AuthOption from "./AuthOption";
 //assets
 import astro from "@/images/astro.png";
 
+const GitConnectorButton = ({ provider, setLoading, loading, handle }: any) => {
+  return (
+    <Button
+      justifyContent="space-between"
+      width="100%"
+      onClick={() => {
+        handle()
+          .then(() => {
+            setLoading(true);
+          })
+          .catch(() => {
+            setLoading(false);
+          });
+      }}
+    >
+      {loading ? "Loading..." : `Sign In With ${provider}`}
+      <BsGithub />
+    </Button>
+  );
+};
+
 const Auth = () => {
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -31,22 +52,25 @@ const Auth = () => {
     <Box className="w-[400px] rounded-lg overflow-hidden text-slate-400 p-5 flex flex-col border border-blue-800/40 shadow-2xl shadow-blue-900/30">
       <Header />
       <VStack spacing={2} mt={3} width="100%" alignItems="flex-start">
-        <Button
-          justifyContent="space-between"
-          width="100%"
-          onClick={() => {
-            handleSignInWithGitHub()
-              .then(() => {
-                setLoading(true);
-              })
-              .catch(() => {
-                setLoading(false);
-              });
-          }}
-        >
-          {loading ? "Loading..." : "Sign In With GitHub"}
-          <BsGithub />
-        </Button>
+        <GitConnectorButton
+          provider="Github"
+          setLoading={setLoading}
+          loading={loading}
+          handle={handleSignInWithGitHub}
+        />
+        <GitConnectorButton
+          provider="GitLab"
+          setLoading={setLoading}
+          loading={loading}
+          handle={handleSignInWithGitHub}
+        />
+        <GitConnectorButton
+          provider="BitBucket"
+          setLoading={setLoading}
+          loading={loading}
+          handle={handleSignInWithGitHub}
+        />
+
         <Heading size="xs" mt="2">
           New here?
         </Heading>
--- app/repos/Settings.tsx
+++ app/repos/Settings.tsx
@@ -5,40 +5,42 @@ import { Box, Divider, Input, VStack, Text } from "@chakra-ui/react";
 import { useSessionContext } from "@/context/useSessionContext";
 
 const Repos = () => {
-    const { user, methods } = useSessionContext();
-    // const [context, setContext] = useState<any>(null);
-    // const [branch, setBranch] = useState<any>(null);
-    // const [tech, setTech] = useState<any>(null);
+    const { user, methods, context, techStack, branch } = useSessionContext();
 
     if (!user) return null;
 
     return (
         <>
             <Box mt={6} mb={2}>
-                <VStack spacing={2} mt={2} alignItems='flex-start'>
+                <VStack spacing={2} mt={2} alignItems="flex-start">
                     <Text>Branch Name</Text>
                     <Input
-                        // value={branch}
+                        value={branch}
                         placeholder="main"
-                        onChange={(e: any) => methods.setBranch(e?.value)}
+                        onChange={(e: any) => {
+                            methods.setBranch(e?.value);
+                        }}
                         border="0.5px solid gray"
                     />
-                    {/* <Text>Tech Stack</Text>
+                    <Text>Tech Stack</Text>
                     <Input
-                        // value={tech}
+                        value={techStack}
                         placeholder="What tech stack are you using? E.g. React, Next.js, Chakra UI"
-                        onChange={(e: any) => methods.setTechStack(e?.value)}
+                        onChange={(e: any) => {
+                            methods.setTechStack(e?.value);
+                        }}
                         border="0.5px solid gray"
-                    /> */}
-                    {/* <Text>Context</Text>
+                    />
+                    <Text>Context</Text>
                     <Input
-                        // value={context}
+                        value={context}
                         placeholder="What are you trying to do? E.g. Create a new Next.js booking app"
-                        onChange={(e: any) => methods.setContext(e?.value)}
+                        onChange={(e: any) => {
+                            methods.setContext(e?.value);
+                        }}
                         border="0.5px solid gray"
-                    /> */}
+                    />
                 </VStack>
-
             </Box>
         </>
     );
