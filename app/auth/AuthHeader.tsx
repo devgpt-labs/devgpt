39807177f9f4a30607c3a23945d35956050f2b41
Ash <tom@feb.co.uk>
2023-09-21 08:52:56 -0700

    Added banner

--- app/auth/AuthHeader.tsx
+++ app/auth/AuthHeader.tsx
@@ -1,5 +1,5 @@
 import { supabase } from "@/utils/supabase";
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Tag, Text } from "@chakra-ui/react";
 import AstroHead from "@/images/astro_profile.png";
 
 //components
@@ -15,7 +15,28 @@ export const Header = () => {
       alignItems="center"
     >
       <Logo />
-      <Image src={AstroHead.src} objectFit='contain' width='25px' height='25px' alt="DevGPT Astro" />
+      <Tag
+        p={2}
+        colorScheme="blue"
+        justifyContent="center"
+        position="absolute"
+        top={0}
+        left={0}
+        width="100vw"
+        flexDirection="row"
+      >
+        <Text mr={3}>We are now a web based AI dev-agent. </Text>
+        <Text mr={3}>🎉 </Text>
+        <Text>GitLab and BitBucket connectors coming soon.</Text>
+        {/* <Text>Read more about why we thought this was important</Text> */}
+      </Tag>
+      <Image
+        src={AstroHead.src}
+        objectFit="contain"
+        width="25px"
+        height="25px"
+        alt="DevGPT Astro"
+      />
     </Flex>
   );
 };
