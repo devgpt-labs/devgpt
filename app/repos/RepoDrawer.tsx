de533b20c027e40b3eca5f1e585ca8e9d9c04529
Tylor Mayfield <tylor@tsohg.com>
2023-09-22 19:06:02 -0400

    [Bug] Repo drawer infinite loading
    Add some saftey to hopefully prevent the repo drawer from getting stuck loading forever

--- app/repos/RepoDrawer.tsx
+++ app/repos/RepoDrawer.tsx
@@ -15,8 +15,6 @@ import {
 } from "@chakra-ui/react";
 import { useSessionContext } from "@/context/useSessionContext";
 import getRepos from "@/utils/github/getRepos";
-
-//components
 import Loader from "@/app/components/Loader";
 
 const RepoDrawer = () => {
@@ -25,6 +23,8 @@ const RepoDrawer = () => {
   });
   const { repo, methods, session, user, repoWindowOpen } = useSessionContext();
   const [repos, setRepos] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const btnRef = useRef<any>();
 
   useEffect(() => {
@@ -33,25 +33,34 @@ const RepoDrawer = () => {
   }, [repoWindowOpen]);
 
   useEffect(() => {
+    if (!user) return;
+    setLoading(true);
+    setError(null);
+
     getRepos(session?.provider_token)
       .then((allRepos) => {
         setRepos(allRepos);
       })
       .catch((err) => {
-        console.log({ err });
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  }, [user]);
+  }, [user, session?.provider_token]);
 
-  if (!user) {
-    return null;
-  }
+  const handleCloseDrawer = () => {
+    onClose();
+    setError(null);
+  };
 
   return (
     <>
       <Drawer
         isOpen={isOpen}
         placement="right"
-        onClose={onClose}
+        onClose={handleCloseDrawer}
         finalFocusRef={btnRef}
       >
         <DrawerOverlay />
@@ -59,47 +68,53 @@ const RepoDrawer = () => {
           <DrawerCloseButton />
           <DrawerHeader>Select a repo</DrawerHeader>
           <DrawerBody>
-            {repos?.length > 0 ? (
-              repos?.map((repoOption, index) => {
-                return (
-                  <Flex
-                    key={repoOption.name + repoOption.owner.login}
-                    my={2}
-                    flexDirection="row"
-                    justifyContent={"space-between"}
-                    alignItems={"center"}
-                  >
-                    <Flex flexDirection="column">
-                      <Text fontSize={16}>
-                        {repoOption.name.substring(0, 16)}
-                        {repoOption.name.length > 16 && "..."}
-                      </Text>
-
-                      <Text fontSize={12} color="gray">
-                        {repoOption.owner.login}
-                      </Text>
-                    </Flex>
-
-                    <Button
-                      size="sm"
-                      onClick={() => {
-                        onClose();
-                        methods.setRepo({
-                          owner: repoOption.owner.login,
-                          repo: repoOption.name,
-                        });
-                      }}
-                    >
-                      {repo.repo === repoOption.name &&
-                      repo.owner === repoOption.owner.login
-                        ? "Selected"
-                        : "Select"}
-                    </Button>
-                  </Flex>
-                );
-              })
-            ) : (
+            {loading ? (
               <Loader />
+            ) : error ? (
+              <Text color="red.500">Error loading repositories. Please try again later.</Text>
+            ) : (
+              repos?.length > 0 ? (
+                repos?.map((repoOption, index) => {
+                  return (
+                    <Flex
+                      key={repoOption.name + repoOption.owner.login}
+                      my={2}
+                      flexDirection="row"
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                    >
+                      <Flex flexDirection="column">
+                        <Text fontSize={16}>
+                          {repoOption.name.substring(0, 16)}
+                          {repoOption.name.length > 16 && "..."}
+                        </Text>
+  
+                        <Text fontSize={12} color="gray">
+                          {repoOption.owner.login}
+                        </Text>
+                      </Flex>
+  
+                      <Button
+                        size="sm"
+                        onClick={() => {
+                          onClose();
+                          methods.setRepo({
+                            owner: repoOption.owner.login,
+                            repo: repoOption.name,
+                          });
+                        }}
+                      >
+                        {repo.repo === repoOption.name &&
+                        repo.owner === repoOption.owner.login
+                          ? "Selected"
+                          : "Select"}
+                      </Button>
+                    </Flex>
+                  );
+                })
+              ) : (
+                <Text>No repositories available.</Text>
+              )
             )}
           </DrawerBody>
           <DrawerFooter />
