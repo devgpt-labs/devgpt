89e732fa61d901cd5fdd452433ec5ee7c630498b
Ash Lewis <ashlewis1001@gmail.com>
2023-09-22 19:58:47 -0700

    models are now retrained when a new repo is selected

--- utils/github/getDefaultBranch.ts
+++ utils/github/getDefaultBranch.ts
@@ -1,25 +1,25 @@
 const getDefaultBranch = async (owner: any, repo: any, token: any) => {
-	const url = `https://api.github.com/repos/${owner}/${repo}`;
+  const url = `https://api.github.com/repos/${owner}/${repo}`;
 
-	const options = {
-		method: "GET",
-		headers: {
-			Accept: "application/vnd.github+json",
-			Authorization: `token ${token}`,
-		},
-	};
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `token ${token}`,
+    },
+  };
 
-	try {
-		const response = await fetch(url, options);
-		const data = await response.json();
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
 
-		// If GitHub API returns a message (like errors), then throw an Error
-		if (data.message) throw new Error(data.message);
+    // If GitHub API returns a message (like errors), then throw an Error
+    if (data.message) throw new Error(data.message);
 
-		return data.default_branch;
-	} catch (error) {
-		console.log(`Fsailed to get the default branch: ${error}`);
-	}
+    return data.default_branch;
+  } catch (error) {
+    console.log(`Failed to get the default branch: ${error}`);
+  }
 };
 
 export default getDefaultBranch;
