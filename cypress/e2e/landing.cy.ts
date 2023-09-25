890e056f8da620285327b1e7d8676bb806e767d1
Tylor Mayfield <tylor@tsohg.com>
2023-09-25 19:14:31 -0400

    findByRole missing types

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -1,29 +1,32 @@
-describe('Verify the menu navigation links on the landing screen', () => {
+describe("Verify the menu navigation links on the landing screen", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000'); 
+    cy.visit("http://localhost:3000");
   });
 
-  it('Check for Discord link', () => {
-    cy.fixture('navigationLinks.json').then((menuLinks) => {
-      cy.findByRole('link', { name: /discord/i })
-        .should('exist')
-        .and('have.attr', 'href', menuLinks.discord);
+  it("Check for Discord link", () => {
+    cy.fixture("navigationLinks.json").then((menuLinks) => {
+      //@ts-ignore
+      cy.findByRole("link", { name: /discord/i })
+        .should("exist")
+        .and("have.attr", "href", menuLinks.discord);
     });
   });
 
-  it('Check for Docs link', () => {
-    cy.fixture('navigationLinks.json').then((menuLinks) => {
-      cy.findByRole('link', { name: /docs/i })
-        .should('exist')
-        .and('have.attr', 'href', menuLinks.docs);
+  it("Check for Docs link", () => {
+    cy.fixture("navigationLinks.json").then((menuLinks) => {
+      //@ts-ignore
+      cy.findByRole("link", { name: /docs/i })
+        .should("exist")
+        .and("have.attr", "href", menuLinks.docs);
     });
   });
 
-  it('Check for Github link', () => {
-    cy.fixture('navigationLinks.json').then((menuLinks) => {
-      cy.findByRole('link', { name: /github/i })
-        .should('exist')
-        .and('have.attr', 'href', menuLinks.github);
+  it("Check for Github link", () => {
+    cy.fixture("navigationLinks.json").then((menuLinks) => {
+      //@ts-ignore
+      cy.findByRole("link", { name: /github/i })
+        .should("exist")
+        .and("have.attr", "href", menuLinks.github);
     });
   });
 });
