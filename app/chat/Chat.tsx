b20fd0ef86de4c32888aa36d1c3c846039f0f898
Tom Lewis <73704914+tom-lewis-code@users.noreply.github.com>
2023-09-27 09:00:46 -0700

    Update Chat.tsx

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -55,7 +55,7 @@ const Chat = () => {
     const tokenLimit = await getTokenLimit(user.email);
     const isPro = await checkIfPro(user.email);
 
-    if (promptCount > 75 && !isPro) {
+    if (!isPro && promptCount > 9) {
       setIsLoading(false);
       setFailMessage(
         "You have reached your prompt limit for today, upgrade or check back tomorrow!"
