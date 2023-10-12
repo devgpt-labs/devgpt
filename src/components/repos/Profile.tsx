7381a15d384de19d7078ce38845c13e6c3df2f62
DevVali <90271108+DevVali@users.noreply.github.com>
2023-10-12 19:40:35 +0200

    Update the Report An Issue link

--- src/components/repos/Profile.tsx
+++ src/components/repos/Profile.tsx
@@ -253,7 +253,7 @@ const Profile = () => {
               <Tooltip label="Report An Issue" placement="top">
                 <Link
                   isExternal
-                  href="https://github.com/february-labs/devgpt-releases/issues"
+                  href="https://github.com/devgpt-labs/devgpt-releases/issues"
                 >
                   <IconButton
                     _hover={{
