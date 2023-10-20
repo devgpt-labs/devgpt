47add9c557c39a430d2e0a66f4aab4a5a8213a3c
Ash <tom@feb.co.uk>
2023-10-20 11:41:47 +0100

    Changed READY FOR PROMPTING badge

--- src/pages/platform/agent/TrainingStatus.tsx
+++ src/pages/platform/agent/TrainingStatus.tsx
@@ -6,7 +6,7 @@ const TrainingStatus = (initialMessages: any) => {
   const { status, credits }: any = authStore();
   if (status?.isOverdue || credits < 0) return null;
 
-  return initialMessages?.length === 0 ? (
+  return initialMessages?.length < 2 ? (
     <Text mb={1}>
       Your AI model is <Badge>Training</Badge>, until this is done the AI
       won't be able to access your repos context.
