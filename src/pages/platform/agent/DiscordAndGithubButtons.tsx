c4bd37df60d43bbac98b85238c9ccfb2bc1ccd79
Ash <tom@feb.co.uk>
2023-10-20 11:40:07 +0100

    Updated github stars

--- src/pages/platform/agent/DiscordAndGithubButtons.tsx
+++ src/pages/platform/agent/DiscordAndGithubButtons.tsx
@@ -48,7 +48,7 @@ const DiscordAndGithubButtons = () => {
                 <BsGithub />
                 <BsStars />
                 <Text ml={2} fontSize={14}>
-                  360
+                  364
                 </Text>
               </Flex>
             }
